2c03dc3d106e79dcd912abd46018b97ccb378e42
Hamed Movasaqpoor <hamed.movasaqpoor@gmail.com>
2024-02-27 14:55:29 +0330

    init:v0.1

--- core/settings.py
+++ core/settings.py
@@ -132,11 +132,11 @@ LOCALE_PATHS = (
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 
-# STATICFILES_DIRS = [
-#     os.path.join(PROJECT_DIR, 'static'),
-# ]
+STATICFILES_DIRS = [
+    os.path.join(PROJECT_DIR, 'static'),
+]
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'statics')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, '../media/')
